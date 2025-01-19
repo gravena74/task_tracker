3fa4944da00fa58cb4239cc4f8815d12325960b0
Joao Gravena <gravena74jv@gmail.com>
2025-01-19 19:15:00 -0300

    First commit/middle of the project

---
+++ task_tracker.py
@@ -0,0 +1,70 @@
+import json
+
+disctionary = {
+    "To-do": [
+        {
+            7: "joga RPG",
+            6: "Dar comido pra gamora",
+            5: "Jogar X-Com"
+        }
+    ],
+
+    "In-Progrece": [
+        {
+            4: "Assistindo Modern Family",
+            3: "Joga RPG"
+        }
+    ],
+
+    "Done": [
+        {
+            2: "Jogar Fallout",
+            1: "Se exercitar"
+        }
+    ]
+}
+
+
+def write_task():
+    return
+
+def update_task(type, id, update):
+    with open('manager.json', 'r+') as openfile:
+        json_object = json.load(openfile)
+
+    json_object[type][0][id] = update
+
+    with open("manager.json", "w") as file:
+        json.dump(json_object, file, indent=4)
+    
+def delete_task(type, id):
+    with open('manager.json', 'r+') as openfile:
+        json_object = json.load(openfile)
+
+    del json_object[type][0][id]
+
+    with open("manager.json", "w") as openfile:
+        json.dump(json_object, openfile, indent=4)
+
+def add_task(new_task):
+    with open("manager.json", "r+") as openfile:
+        json_object = json.load(openfile)
+    
+    ultimo_id = int(list(json_object["To-do"][0].keys())[-1])
+    ultimo_id += 1
+    print(ultimo_id)
+
+    json_object["To-do"][0][ultimo_id] = new_task
+    
+    with open("manager.json", "w") as file:
+        json.dump(json_object, file, indent=4)
+    
+
+
+def read_task(type, id):
+    with open('manager.json', 'r') as openfile:
+        json_object = json.load(openfile)
+        return json_object[type][0][id]
+    
+
+add_task("Tarefa de To-do")

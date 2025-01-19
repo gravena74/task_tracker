3ea03924daaffed5131d8ae06339c8b26898bb95
Joao Gravena <gravena74jv@gmail.com>
2025-01-19 20:19:12 -0300

    Second commit, class implementation and organize the code.

--- task_tracker.py
+++ task_tracker.py
@@ -25,46 +25,93 @@ disctionary = {
 }
 
 
-def write_task():
-    return
+# def write_task():
+#     return
 
-def update_task(type, id, update):
-    with open('manager.json', 'r+') as openfile:
-        json_object = json.load(openfile)
+# def update_task(type, id, update):
+#     with open('manager.json', 'r+') as openfile:
+#         json_object = json.load(openfile)
 
-    json_object[type][0][id] = update
+#     json_object[type][0][id] = update
 
-    with open("manager.json", "w") as file:
-        json.dump(json_object, file, indent=4)
+#     with open("manager.json", "w") as file:
+#         json.dump(json_object, file, indent=4)
     
-def delete_task(type, id):
-    with open('manager.json', 'r+') as openfile:
-        json_object = json.load(openfile)
+# def delete_task(type, id):
+#     with open('manager.json', 'r+') as openfile:
+#         json_object = json.load(openfile)
 
-    del json_object[type][0][id]
+#     del json_object[type][0][id]
 
-    with open("manager.json", "w") as openfile:
-        json.dump(json_object, openfile, indent=4)
+#     with open("manager.json", "w") as openfile:
+#         json.dump(json_object, openfile, indent=4)
 
-def add_task(new_task):
-    with open("manager.json", "r+") as openfile:
-        json_object = json.load(openfile)
+# def add_task(new_task):
+#     with open("manager.json", "r+") as openfile:
+#         json_object = json.load(openfile)
     
-    ultimo_id = int(list(json_object["To-do"][0].keys())[-1])
-    ultimo_id += 1
-    print(ultimo_id)
+#     ultimo_id = int(list(json_object["To-do"][0].keys())[-1])
+#     ultimo_id += 1
+#     print(ultimo_id)
 
-    json_object["To-do"][0][ultimo_id] = new_task
+#     json_object["To-do"][0][ultimo_id] = new_task
     
-    with open("manager.json", "w") as file:
-        json.dump(json_object, file, indent=4)
-    
-
-
-def read_task(type, id):
-    with open('manager.json', 'r') as openfile:
-        json_object = json.load(openfile)
-        return json_object[type][0][id]
+#     with open("manager.json", "w") as file:
+#         json.dump(json_object, file, indent=4)
     
 
-add_task("Tarefa de To-do")
+
+# def read_task(type, id):
+#     with open('manager.json', 'r') as openfile:
+#         json_object = json.load(openfile)
+#         return json_object[type][0][id]
+    
+
+# add_task("Tarefa de To-do")
+
+
+class TaskTracker:
+    def __init__(self, type, id):
+        self.type = type
+        self.id = id
+
+    def getAllTask(self):
+        print("mdoamdoa")
+
+    def updateTask(self, update):
+        with open('manager.json', 'r+') as openfile:
+            json_object = json.load(openfile)
+
+        json_object[self.type][0][self.id] = update
+
+        with open("manager.json", "w") as file:
+            json.dump(json_object, file, indent=4)
+
+    def deleteTask(self):
+        with open('manager.json', 'r+') as openfile:
+            json_object = json.load(openfile)
+
+        del json_object[self.type][0][self.id]
+
+        with open("manager.json", "w") as openfile:
+            json.dump(json_object, openfile, indent=4)
+
+
+    def addTask(newTask):
+         with open("manager.json", "r+") as openfile:
+            json_object = json.load(openfile)
+        
+            ultimo_id = int(list(json_object["To-do"][0].keys())[-1])
+            ultimo_id += 1
+
+            json_object["To-do"][0][ultimo_id] = newTask
+            
+            with open("manager.json", "w") as file:
+                json.dump(json_object, file, indent=4)
+
+    def read_task(self):
+        with open('manager.json', 'r') as openfile:
+            json_object = json.load(openfile)
+            return json_object[self.type][0][self.id]
+    
+print(TaskTracker("To-do", "11").read_task())
